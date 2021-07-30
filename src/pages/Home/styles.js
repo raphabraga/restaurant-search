6203b96795aea2c231b354b12d2ba91b973d9b9c
raphabraga <raphael-braga@hotmail.com>
2021-07-29 23:50:18 -0300

    added the styled-reset module in order to reset default browser styles configuration

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-  background-color: red;
+  background-color: ${(props) => props.theme.colors.primary};
 `;
 
 export default Container;
