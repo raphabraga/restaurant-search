525469ba2f44747055dfed0e6a9aff05bb39223f
raphabraga <raphael-braga@hotmail.com>
2021-07-30 02:57:16 -0300

    implemented text field using material UI for react. downloaded module @material/react-text-field and imported css in main index.js

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, SearchBox } from "./styles";
-import logo from "../../assets/logo4.svg";
+import logo from "../../assets/logo.svg";
+import TextField, { Input } from "@material/react-text-field";
 
-const Home = () => (
-  <Container>
-    <SearchBox>
-      <img src={logo} alt="Restaurant Finder application logo" />
-    </SearchBox>
-  </Container>
-);
-
+const Home = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  return (
+    <Container>
+      <SearchBox>
+        <img src={logo} alt="Restaurant Finder application logo" />
+        <TextField label="Type your search" outlined>
+          <Input
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </TextField>
+      </SearchBox>
+    </Container>
+  );
+};
 export default Home;
